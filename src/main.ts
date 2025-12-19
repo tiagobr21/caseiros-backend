f8b41294fb20bed87cd1fb7c82e57be25a883e8e
Tiago Souza <tiagobrr17@gmail.com>
2025-12-19 17:23:24 -0400

    system ok

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(4001);
+  await app.listen(4000);
   console.log('🚀 Backend rodando em http://localhost:4000');
 }
 bootstrap();
